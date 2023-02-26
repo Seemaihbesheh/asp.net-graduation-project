b87a5130e8ae474774684c83dc620d1a6cc58dc2
SeemaIHB <ihbsema@gmail.com>
2023-02-26 23:15:44 +0200

    try1

---
+++ WebApplication3/DBContext/post.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.DBContext
+{
+    public class post
+    {
+        [Key]
+        
+
+        public int Id { get; set; }
+
+        public string text { get; set; }
+      
+        public bool flag_image { get; set; }
+
+        public int User_id { get; set; }
+
+       
+        public int image_id { get; set; }
+    }
+}
